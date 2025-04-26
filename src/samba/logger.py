3638f8fa3d4c556426790a857b866f13bf25effe
4LAR <nikitstolarov@gmail.com>
2025-04-27 02:26:27 +0300

    Neiro optimization

--- src/samba/logger.py
+++ src/samba/logger.py
@@ -1,10 +1,7 @@
-from shell import Shell
 from datetime import datetime
-import subprocess
-import sys
-import time
-import re
 import json
+import time
+from shell import Shell
 
 class SambaLogger(Shell):
     sessions = {}
@@ -14,69 +11,59 @@ class SambaLogger(Shell):
         pass
 
     def _get_status_json(self):
-        return json.loads(self._run_command_output(
-            ['smbstatus', '-j']
-        ))
+        return json.loads(self._run_command_output(['smbstatus', '-j']))
+
+    def _log_event(self, event_type, details):
+        timestamp = f"[ {datetime.now().strftime('%d.%m.%Y %H:%M:%S')} ]"
+        print(timestamp, details)
+
+    def _track_objects(self, current_objects, stored_objects, object_type):
+        # Handle new objects
+        for obj_id, obj_data in current_objects.items():
+            if obj_id not in stored_objects:
+                stored_objects[obj_id] = obj_data
+                self._log_event(
+                    f"new_{object_type}",
+                    self._get_object_message(object_type, 'opened', obj_id, obj_data)
+                )
+
+        # Handle closed objects
+        closed_objects = set(stored_objects) - set(current_objects)
+        for obj_id in closed_objects:
+            obj_data = stored_objects.pop(obj_id)
+            self._log_event(
+                f"closed_{object_type}",
+                self._get_object_message(object_type, 'closed', obj_id, obj_data)
+            )
+
+    def _get_object_message(self, object_type, action, obj_id, obj_data):
+        if object_type == 'tcons':
+            return {
+                'opened': f"Opened new connection: {obj_data['machine']} ({obj_id}) to '{obj_data['service']}'",
+                'closed': f"Disconnected: {obj_data['machine']} ({obj_id}) authorized as '{obj_data['service']}'"
+            }[action]
+        elif object_type == 'sessions':
+            return {
+                'opened': f"Created new session: {obj_data['remote_machine']} ({obj_id}) authorized as '{obj_data['username']}'",
+                'closed': f"Removed session: {obj_data['remote_machine']} ({obj_id}) authorized as '{obj_data['username']}'"
+            }[action]
+        elif object_type == 'open_files':
+            return {
+                'opened': f"Open file '{obj_data['filename']}' in '{obj_data['service_path'].split('/')[-1]}'",
+                'closed': f"Close file '{obj_data['filename']}' in '{obj_data['service_path'].split('/')[-1]}'"
+            }[action]
+        return ""
 
     def monitor(self, delay=2):
         while True:
-            data = self._get_status_json()
-            # print(json.dumps(data, indent=4))
+            try:
+                data = self._get_status_json()
 
-            date_now = f"[ {datetime.now().strftime('%d.%m.%Y %H:%M:%S')} ]"
+                self._track_objects(data.get('tcons', {}), self.tcons, 'tcons')
+                self._track_objects(data.get('sessions', {}), self.sessions, 'sessions')
+                self._track_objects(data.get('open_files', {}), self.open_files, 'open_files')
 
-            # Соединения
-            tcons = data["tcons"]
-            for con in tcons:
-                if not (con in self.tcons.keys()):
-                    con_data = tcons[con]
-                    self.tcons[con] = {
-                        "service": con_data['service'],
-                        "machine": con_data['machine']
-                    }
-
-                    print(date_now, f"Opened new connection: {con_data['machine']} ({con}) to '{con_data['service']}'")
-
-            closed_tcons = list(set(self.tcons.keys()) - set(tcons.keys()))
-            for con in closed_tcons:
-                con_data = self.tcons[con]
-                print(date_now, f"Desconnected: {con_data['machine']} ({con}) authorized as '{con_data['service']}'")
-                self.tcons.pop(con)
-
-            # Сессии
-            sessions = data["sessions"]
-            for ses in sessions:
-                if not (ses in self.sessions.keys()):
-                    ses_data = sessions[ses]
-                    self.sessions[ses] = {
-                        "remote_machine": ses_data['remote_machine'],
-                        "username": ses_data['username']
-                    }
-
-                    print(date_now, f"Created new session: {ses_data['remote_machine']} ({ses}) authorized as '{ses_data['username']}'")
-
-            closed_sessions = list(set(self.sessions.keys()) - set(sessions.keys()))
-            for ses in closed_sessions:
-                ses_data = self.sessions[ses]
-                print(date_now, f"Removed session: {ses_data['remote_machine']} ({ses}) authorized as '{ses_data['username']}'")
-                self.sessions.pop(ses)
-
-            # Открытые файлы
-            open_files = data["open_files"]
-            for file in open_files:
-                if not (file in self.open_files.keys()):
-                    file_data = open_files[file]
-                    self.open_files[file] = {
-                        "service_path": file_data['service_path'],
-                        "filename": file_data['filename']
-                    }
-
-                    print(date_now, f"Open file '{file_data['filename']}' in '{file_data['service_path'].split('/')[-1]}'")
-
-            closed_open_files = list(set(self.open_files.keys()) - set(open_files.keys()))
-            for file in closed_open_files:
-                file_data = self.open_files[file]
-                print(date_now, f"Close file '{file_data['filename']}' in '{file_data['service_path'].split('/')[-1]}'")
-                self.open_files.pop(file)
+            except Exception as e:
+                self._log_event('error', f"Error in monitoring: {str(e)}")
 
             time.sleep(delay)
